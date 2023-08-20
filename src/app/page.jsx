41db994f27774e1a9eba85a962b9ffaa49297570
Aman <amangarg0303@gmail.com>
2023-08-20 22:48:57 +0530

    created home header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
+import HomeHeader from "@/components/HomeHeader";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h2 className="bg-blue-400">Home</h2>
-    </div>
+    <>
+      <HomeHeader />
+    </>
   );
 };
 
