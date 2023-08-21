f8f513a6347747eedf04c7a97433f612e7c13f5f
Aman <amangarg0303@gmail.com>
2023-08-21 15:51:28 +0530

    added realtime search

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const WebSearchPage = () => {
-  return <div>Web search page</div>;
+const WebSearchPage = async ({ searchParams }) => {
+  const res = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+
+  const data = await res.json();
+
+  const result = await data?.items;
+
+  return <>{result && result.map((r) => <h1 key={r.id}>{r.title}</h1>)}</>;
 };
 
 export default WebSearchPage;
