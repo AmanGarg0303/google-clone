a46616a1796d4599bff540a152226226192f6518
Aman <amangarg0303@gmail.com>
2023-08-21 16:16:03 +0530

    handled errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const WebSearchPage = async ({ searchParams }) => {
@@ -5,9 +6,26 @@ const WebSearchPage = async ({ searchParams }) => {
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
-  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Something went wrong!");
+  }
 
+  const data = await res.json();
   const result = await data?.items;
+
+  if (!result) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
 
   return <>{result && result.map((r) => <h1 key={r.id}>{r.title}</h1>)}</>;
 };
